9192f2f541e037af8574a31de243d84ed1280d88
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-06 01:42:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["Bob", "Linda", "Tina", "Gene"]
+  belcher_fam = ["Bob", "Linda", "Tina", "Gene"]
   << "Louise"
   end
 
