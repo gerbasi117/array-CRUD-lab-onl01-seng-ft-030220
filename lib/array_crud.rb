40f3d7bddb6f3efff7dec3569d222298f3b54538
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-06 02:35:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,11 +29,11 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  am
 end
 
 def retrieve_first_element_from_array(array)
-  
+wow  
 end
 
 def retrieve_last_element_from_array(array)
