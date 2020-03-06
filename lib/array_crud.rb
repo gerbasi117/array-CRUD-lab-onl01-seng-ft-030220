393fc125f37988f70fb7dac5c9ff956be0d599e7
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-06 02:21:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   ["Homer", "Marge", "Bart", "Lisa"]
 end
 
-def add_element_to_end_of_array(array)
+def add_element_to_end_of_array(array,element)
   array = []
   array << "Louise"
 end
